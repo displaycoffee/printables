9f2ad54cf6642457fb898463524046f2e9864dec
displaycoffee <adria.m.murphy@gmail.com>
2026-08-21 12:20:27 -0600

    Add layout for labels

--- src/layout/container/Container.tsx
+++ src/layout/container/Container.tsx
@@ -4,14 +4,20 @@ import './styles/container.scss';
 /* Packages */
 import { Link } from 'react-router-dom';
 
+/* Scripts */
+import { alphabet } from './scripts/alphabet';
+
 /* Components */
 import { ErrorBoundary } from '../../components/error-boundary/ErrorBoundary';
+import { Labels } from '../../components/labels/Labels';
 
 export const Container = () => {
 	return (
 		<ErrorBoundary message={<ContainerError />}>
 			<div className="page">
-				<div className="page-content">Stuff goes here.</div>
+				<div className="page-content">
+					<Labels label={'Art Series'} values={alphabet} />
+				</div>
 			</div>
 		</ErrorBoundary>
 	);
