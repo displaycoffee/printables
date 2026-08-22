173cd9a12d9c9f65343d5b0550795b7134ec708a
displaycoffee <adria.m.murphy@gmail.com>
2026-08-22 09:10:43 -0600

    add top loaders

--- src/components/navigation/scripts/navigation.ts
+++ src/components/navigation/scripts/navigation.ts
@@ -6,6 +6,7 @@ import { NavigationType } from './navigation-types';
 
 /* Components */
 const Labels = lazy(() => import('../../../pages/labels/Labels').then((m) => ({ default: m.Labels })));
+const TopLoaders = lazy(() => import('../../../pages/top-loaders/TopLoaders').then((m) => ({ default: m.TopLoaders })));
 
 export const navigation: NavigationType[] = [
 	{
@@ -13,6 +14,14 @@ export const navigation: NavigationType[] = [
 		element: Labels,
 		isRoute: true,
 		label: 'Labels',
+		showInNav: true,
+		url: '/',
+	},
+	{
+		id: 1,
+		element: TopLoaders,
+		isRoute: true,
+		label: 'Top Loaders',
 		showInNav: true,
 		url: '/',
 	},
