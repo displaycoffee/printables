c06ad90cebd230d0c0fe53440352bf27fc941d9b
Adria <displaycoffee@users.noreply.github.com>
2026-09-18 10:05:20 -0600

    Update vite.sitemap.js

--- vite.sitemap.js
+++ vite.sitemap.js
@@ -1,4 +1,7 @@
+/* Scripts */
 import packageJSON from './package.json' with { type: 'json' };
+
+/* Variables */
 const hostname = packageJSON.homepage || 'https://localhost:3000';
 const location = new URL(hostname);
 
@@ -6,10 +9,9 @@ let sitemap = {
 	hostname: location.origin,
 	readable: true,
 	exclude: ['/assets', '/assets/css', '/assets/fonts', '/assets/images', '/assets/images/test', '/assets/images/theme', '/assets/js'],
-	dynamicRoutes: ['/page-one', '/page-two', '/page-two/child-page-one', '/page-two/child-page-two'],
+	dynamicRoutes: ['/mtg-divider-labels', '/mtg-drawer-labels', '/mtg-top-loaders'],
 };
-if (location?.pathname && location.pathname != '/') {
-	sitemap.basePath = location.pathname;
-}
+
+if (location?.pathname && location.pathname != '/') sitemap.basePath = location.pathname;
 
 export const sitemapConfig = sitemap;
