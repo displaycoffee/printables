053993ad323bc205a169415d06562f8cc0568c85
Adria <displaycoffee@users.noreply.github.com>
2026-09-18 10:27:46 -0600

    Update variables.ts

--- src/_core/scripts/variables.ts
+++ src/_core/scripts/variables.ts
@@ -2,6 +2,6 @@
 const directory = '/printables';
 export const variables: VariablesType = {
 	paths: {
-		basename: window.location.pathname.includes(directory) ? directory : '',
+		basename: typeof window == 'object' && window.location.pathname.includes(directory) ? directory : '',
 	},
 };
